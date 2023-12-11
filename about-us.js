b6a049979ec54a68f9a402a8de7d2d479b46e8d8
manikanta <manikantamnabde@gmail.com>
2023-12-11 11:36:12 +0530

    common.js from commmon function

--- about-us.js
+++ about-us.js
@@ -1,71 +1,80 @@
-//------------------------Firebase Config-----------------------
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-import { ref, uploadBytes, getDownloadURL, getStorage } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js";
+//------------------------Firebase Config-----------------------//
+// firesStore
 import {
-    getFirestore,
+    firestore,
     collection,
     query,
     where,
     getDocs,
+    updateDoc,
     doc,
     getDoc,
-    setDoc,
-    onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+    addDoc
+} from "./assets/repository/initialize.js";
+
+//Auth 
 import {
-    getAuth,
-    signOut,
+    auth,
     onAuthStateChanged,
+    signOut,
     updatePassword,
     EmailAuthProvider,
     reauthenticateWithCredential,
-} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+    signInWithEmailAndPassword,
+    sendPasswordResetEmail,
+    createUserWithEmailAndPassword
+} from "./assets/repository/initialize.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBrIAlkIyp5ALsv5RslbXA1oQVQL3eKhig",
-    authDomain: "pharma-ecom-app.firebaseapp.com",
-    projectId: "pharma-ecom-app",
-    storageBucket: "pharma-ecom-app.appspot.com",
-    messagingSenderId: "798776981223",
-    appId: "1:798776981223:web:16f92da76fe7c2f1cf9442"
-};
+// storage
+import {
+    storage,
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "./assets/repository/initialize.js"
 
-//global
+import { 
+    updateProfileName,
+    updateProfilePicture,
+    displayMessage,
+    fetchNavCategories,
+    roleAccess,
+    onLoggedIn,
+    onLoggedOut
+    } from "./assets/repository/common/common.js";
+//------------------------------------------------------------------//
+
+//------------------------------global variable--------------------//
 var userData = null;
-
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
 const confirmLogoutBtn = document.getElementById("confirmLogoutBtn");
 var loggedIn = false;
+//----------------------------------------------------------------//
 
 // Function to check if the user is logged in
 function isUserLoggedIn() {
     return !!auth.currentUser;
 }
 
-//***********************************event listener**************************************
-// Add an event listener to the confirmation logout button
+
+/**
+ * logout Eventlistenser
+ * @return mydev
+ */
 confirmLogoutBtn.addEventListener("click", () => {
     console.log("1")
     signOut(auth)
         .then(() => {
-            window.location.href = "login.html"; // Redirect to the login page
+            window.location.href = "login.html";
         })
         .catch((error) => {
             console.error("Error during logout:", error);
         });
 });
 
-//************************cart dependency**********************************
-//update cart function cart(dependency)
 /**
  * 
  * @returns promise
- * 
- * requires: 
- * getCart()
+ * requires: getCart() 
  */
 function updateCart() {
     return new Promise(async (resolve) => {
@@ -86,29 +95,26 @@ function updateCart() {
     })
 }
 
+/**
+ * getCart() function
+ * @returns mydev
+ */
 async function getCart() {
     return new Promise(async (resolve) => {
         if (loggedIn) {
-            console.log("form getCArt()")
             const cartSnapshot = await getDocs(collection(firestore, 'users', auth.currentUser.uid, 'cart'))
-            console.log("form getCArt(1.1)")
             if (cartSnapshot.empty) {
-                console.log("form getCArt(1.2)")
                 resolve([])
             }
-            console.log("form getCArt(1.3)")
             let cart = []
             cartSnapshot.forEach(doc => {
                 cart.push(doc.data())
             })
-            console.log("form getCArt(1.4)")
             resolve(cart)
         }
         else {
-            console.log("form getCArt1)")
             const cartSnapshot = JSON.parse(sessionStorage.getItem('cart'))
             if (!cartSnapshot) {
-                console.log('from true')
                 resolve([])
                 return
             }
@@ -121,7 +127,11 @@ async function getCart() {
     })
 }
 
-//get user snapshot cart(dependency)
+/**
+ * Getting user snapshot or user details
+ * @param {*} uid 
+ * @returns mydev 
+ */
 function getUserSnapshot(uid) {
     const userRef = doc(firestore, 'users', uid)
     console.log('3')
@@ -129,20 +139,23 @@ function getUserSnapshot(uid) {
         resolve(getDoc(userRef))
     })
 }
-//************************************************************************
 
-  // Use onAuthStateChanged to control access to admin dashboard
+/**
+ * onAuthStateChanged firebase service
+ * @returns mydev 
+ * @requires 
+ * roleAccess(role)
+ * 
+ */  
   onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log("if")
-        document.querySelector('#logout-btn').style.display='block';
+       
         loggedIn = true
         onLoggedIn();
-        // User is authenticated
         const docRef = doc(firestore, "users", user.uid);
         const docSnap = getDoc(docRef);
         docSnap.then((docSnapshot) => {
-            // console.log(docSnapshot)
             if (docSnapshot.exists()) {
                 userData = docSnapshot.data();
                 console.log(userData.role);
@@ -151,110 +164,68 @@ function getUserSnapshot(uid) {
                 updateCart();
                 fetchNavCategories();
                 updateProfilePicture(userData.role,userData.profilePicture)
-                // updateCart();
             }
         });
     } else {
         console.log("else");
         updateCart();
         fetchNavCategories();
-        // User is not authenticated, redirect to login page
         // window.location.href = "login.html";
-        document.querySelector('#logout-btn').style.display='none';
+        onLoggedOut()
     }
 });
 
-function updateProfileName(role, fullName) {
-    // Based on the role, select the appropriate element
-    console.log(fullName)
-    let profileNameElement;
-    switch (role) {
-        case 'CUSTOMER':
-            profileNameElement = document.getElementById('customerAppbar').querySelector('.profile-name');
-            break;
-        // case 'AGENT':
-        //     profileNameElement = document.getElementById('agentAppbar').querySelector('.profile-name');
-        //     break;
-        case 'ADMIN':
-            profileNameElement = document.getElementById('adminAppbar').querySelector('.profile-name');
-            break;
-        default:
-            console.error('Unknown role:', role);
-            return;
-    }
-    profileNameElement.textContent = fullName;
-}
+/**
+ * Role access function
+ * @param {*} role 
+ */
+// function roleAccess(role) {
+//     const roleMap = new Map([
+//         ["ADMIN", "adminAppbar"],
+//         ["CUSTOMER", "customerAppbar"],
+//         // ["AGENT", "agentAppbar"],
+//     ]);
+//     const appbarList = document.querySelectorAll(`#${roleMap.get(role)}`);
+//     appbarList.forEach((appbar) => {
+//         appbar.classList.remove("d-none");
+//     })
+// }
 
-function updateProfilePicture(role, profilePicture) {
-    let profilePictureElement;
-    const defaultProfilePicture = 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp';
+/**
+ * post login
+ * @returns mydev
+ */
+// function onLoggedIn() {
+//     console.log("loggedIn")
+//     var navItemList = document.querySelectorAll(".loggedIn");
+//     navItemList.forEach((navItem) => {
+//         navItem.style.display = "block";
+//     });
 
-    switch (role) {
-        case 'CUSTOMER':
-            profilePictureElement = document.getElementById('customerAppbar').querySelector('#profile-picture');
-            break;
-        // case 'AGENT':
-        //     profilePictureElement = document.getElementById('agentAppbar').querySelector('#profile-picture');
-        //     break;
-        case 'ADMIN':
-            profilePictureElement = document.getElementById('adminAppbar').querySelector('#profile-picture');
-            break;
-        default:
-            console.error('Unknown role:', role)
-            return;
-    }
+//     navItemList = document.querySelectorAll(".loggedOut");
+//     navItemList.forEach((navItem) => {
+//         navItem.style.display = "none";
+//     });
+// }
 
-    // Check if profilePicture is empty or undefined
-    if (profilePicture && profilePicture.trim() !== '') {
-        profilePictureElement.src = profilePicture;
-    } else {
-        // Set to the default profile picture if no picture is provided
-        profilePictureElement.src = defaultProfilePicture;
-    }
-}
+/**
+ * pre login
+ * @returns mydev
+ */
+// function onLoggedOut() {
+//     console.log("loggedOut")
+//     var navItemList = document.querySelectorAll(".loggedOut");
+//     navItemList.forEach((navItem) => {
+//         navItem.style.display = "block";
+//     });
 
-function roleAccess(role) {
-    // console.log('inside role')
-    const roleMap = new Map([
-        ["ADMIN", "adminAppbar"],
-        ["CUSTOMER", "customerAppbar"],
-        // ["AGENT", "agentAppbar"],
-    ]);
-    const appbarList = document.querySelectorAll(`#${roleMap.get(role)}`);
-    appbarList.forEach((appbar) => {
-        appbar.classList.remove("d-none");
-    })
-}
+//     navItemList = document.querySelectorAll(".loggedIn");
+//     navItemList.forEach((navItem) => {
+//         navItem.style.display = "none";
+//     });
+// }
 
-//to execut upon logging in
-function onLoggedIn() {
-    console.log("loggedIn")
-    var navItemList = document.querySelectorAll(".loggedIn");
-    navItemList.forEach((navItem) => {
-        navItem.style.display = "block";
-    });
 
-    navItemList = document.querySelectorAll(".loggedOut");
-    navItemList.forEach((navItem) => {
-        navItem.style.display = "none";
-    });
-}
-
-//to execute upon logging out
-function onLoggedOut() {
-    console.log("loggedOut")
-    var navItemList = document.querySelectorAll(".loggedOut");
-    navItemList.forEach((navItem) => {
-        navItem.style.display = "block";
-    });
-
-    navItemList = document.querySelectorAll(".loggedIn");
-    navItemList.forEach((navItem) => {
-        navItem.style.display = "none";
-    });
-}
-
-//*********************************************************************
 /**
  * 
  * @param {*} message 
@@ -262,90 +233,88 @@ function onLoggedOut() {
  * 
  * Toast message
  */
-function displayMessage(message, type) {
-    // Get the toast container element
-    const toastContainer = document.querySelector(".toast-container");
+// function displayMessage(message, type) {
+//     const toastContainer = document.querySelector(".toast-container");
 
-    // Create a clone of the toast template
-    const toast = document.querySelector(".toast").cloneNode(true);
+//     // Create a clone of the toast template
+//     const toast = document.querySelector(".toast").cloneNode(true);
 
-    console.log(toast)
-    // Set the success message
-    toast.querySelector(".compare-note").innerHTML = message;
+//     console.log(toast)
+//     // Set the success message
+//     toast.querySelector(".compare-note").innerHTML = message;
 
-    //set text type  success/danger
-    if (type === "danger") {
-        toast.classList.remove("bg-success");
-        toast.classList.add("bg-danger");
-    } else {
-        toast.classList.add("bg-success");
-        toast.classList.remove("bg-danger");
-    }
+//     //set text type  success/danger
+//     if (type === "danger") {
+//         toast.classList.remove("bg-success");
+//         toast.classList.add("bg-danger");
+//     } else {
+//         toast.classList.add("bg-success");
+//         toast.classList.remove("bg-danger");
+//     }
 
-    // Append the toast to the container
-    toastContainer.appendChild(toast);
+//     // Append the toast to the container
+//     toastContainer.appendChild(toast);
 
-    // Initialize the Bootstrap toast and show it
-    const bsToast = new bootstrap.Toast(toast);
-    bsToast.show();
+//     // Initialize the Bootstrap toast and show it
+//     const bsToast = new bootstrap.Toast(toast);
+//     bsToast.show();
 
-    // Remove the toast after it's closed
-    toast.addEventListener("hidden.bs.toast", function () {
-        toast.remove();
-    });
-}
+//     // Remove the toast after it's closed
+//     toast.addEventListener("hidden.bs.toast", function () {
+//         toast.remove();
+//     });
+// }
 
-//*******************************************************************
 /**
  * 
  * @returns promise
  */
-async function fetchNavCategories() {
-    const categoryList = document.querySelector('.nav-category')
-    const mobileCategoryList = document.querySelector('.mobile-nav-category')
+// async function fetchNavCategories() {
+//     const categoryList = document.querySelector('.nav-category')
+//     const mobileCategoryList = document.querySelector('.mobile-nav-category')
 
-    categoryList.innerHTML = `
-    <div class='w-100 d-flex justify-content-center'>
-        <div class="spinner-grow text-secondary" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-    </div>
-    `
-    mobileCategoryList.innerHTML = `
-    <div class='w-100 d-flex justify-content-center'>
-        <div class="spinner-grow text-secondary" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-    </div>
-    `
-    const categorySnapshot = await getDocs(collection(firestore, 'categories'))
-    if (categorySnapshot.empty) {
-        console.log('from empty')
-        resolve()
-        return
-    }
+//     categoryList.innerHTML = `
+//     <div class='w-100 d-flex justify-content-center'>
+//         <div class="spinner-grow text-secondary" role="status">
+//             <span class="visually-hidden">Loading...</span>
+//         </div>
+//     </div>
+//     `
+//     mobileCategoryList.innerHTML = `
+//     <div class='w-100 d-flex justify-content-center'>
+//         <div class="spinner-grow text-secondary" role="status">
+//             <span class="visually-hidden">Loading...</span>
+//         </div>
+//     </div>
+//     `
+//     const categorySnapshot = await getDocs(collection(firestore, 'categories'))
+//     if (categorySnapshot.empty) {
+//         console.log('from empty')
+//         resolve()
+//         return
+//     }
 
-    categoryList.innerHTML = ``
-    mobileCategoryList.innerHTML = ``
+//     categoryList.innerHTML = ``
+//     mobileCategoryList.innerHTML = ``
 
-    categorySnapshot.forEach(doc => {
-        const span = document.createElement('span')
-        span.innerHTML = `
-        <div class="gi-tab-list nav flex-column nav-pills me-3" id="v-pills-tab"
-        role="tablist" aria-orientation="vertical">
-            <button class="nav-link" id="v-pills-home-tab" data-bs-toggle="pill"
-                data-bs-target="#v-pills-home" type="button" role="tab"
-                aria-controls="v-pills-home" aria-selected="true">
-                <a class="text-decoration-none text-black" href="products.html?categoryId=${doc.data().categoryId}">${doc.data().name}</a>
-            </button>
-        </div>
-        `
-        categoryList.appendChild(span)
+//     categorySnapshot.forEach(doc => {
+//         const span = document.createElement('span')
+//         span.innerHTML = `
+//         <div class="gi-tab-list nav flex-column nav-pills me-3" id="v-pills-tab"
+//         role="tablist" aria-orientation="vertical">
+//             <button class="nav-link" id="v-pills-home-tab" data-bs-toggle="pill"
+//                 data-bs-target="#v-pills-home" type="button" role="tab"
+//                 aria-controls="v-pills-home" aria-selected="true">
+//                 <a class="text-decoration-none text-black" href="products.html?categoryId=${doc.data().categoryId}">${doc.data().name}</a>
+//             </button>
+//         </div>
+//         `
+//         categoryList.appendChild(span)
 
-        const list = document.createElement('li')
-        list.innerHTML = `
-        <a class="text-decoration-none text-black" href="products.html?categoryId=${doc.data().categoryId}">${doc.data().name}</a>
-        `
-        mobileCategoryList.appendChild(list)
-    })
-}
+//         const list = document.createElement('li')
+//         list.innerHTML = `
+//         <a class="text-decoration-none text-black" href="products.html?categoryId=${doc.data().categoryId}">${doc.data().name}</a>
+//         `
+//         mobileCategoryList.appendChild(list)
+//     })
+// }
